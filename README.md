e5d6c33c45060179eef3c5b62f9f5ec07b7e5dcb
denis <denis@genielifesciences.com>
2023-08-28 09:51:45 -0400

    Version 0.2.0

--- README.md
+++ README.md
@@ -15,15 +15,7 @@ Opentrons REST API Server.
 
 # Install on Opentrons
 
-```bash
-export IP=xxx.xxx.xxx.xxx
-ssh root@${IP} 'mount -o remount,rw /'
-ssh root@${IP} 'mkdir -p /data/user_storage/opentronsrestserver'
-scp server/server.py root@${IP}:/data/user_storage/opentronsrestserver
-scp opentrons-rest-server.service root@${IP}:/etc/systemd/system
-ssh root@${IP} 'systemctl enable opentrons-rest-server'
-ssh root@${IP} '/sbin/reboot'
-```
+SSH into the Opentrons system and run `wget -q -O - https://raw.githubusercontent.com/genie-inc/opentrons_rest_api/master/install.sh | sh`
 
 # Restarting the Rest server
 
@@ -36,3 +28,45 @@ ssh root@${IP} '/sbin/reboot'
 - To tail and follow the log for our rest server
   `journalctl -u opentrons-rest-server -f`
 - To just see the log omit the `-f`
+
+# Running server in Simulate mode
+
+Server can be run locally using this command:
+
+```
+cd server
+uvicorn server:app  --host 0.0.0.0 --port 9090
+```
+
+It's not very useful without simulate mode. You need to be in the server directory first.
+
+To start in server in simulate mode create the environment variable OT_DEBUG:
+`export OT_DEBUG=true`
+and then start the server. Opentrons will display some useful info in the log about the simulate process.
+
+To stop simulating just run `unset OT_DEBUG`
+
+# Running in debugger
+
+- Read this: https://fastapi.tiangolo.com/tutorial/debugging/
+- Follow the instructions for vscode.
+- You will probably want to run in simulate mode so will need to set the env var in the launcher:
+
+```
+  "version": "0.2.0",
+  "configurations": [
+    {
+      "name": "Python: Current File",
+      "type": "python",
+      "request": "launch",
+      "program": "${file}",
+      "console": "integratedTerminal",
+      "env": {
+        "OT_DEBUG": "true"
+      }
+    }
+  ]
+}
+```
+
+- don't forget to remove the uvicorn stuff from the file before merging!
---
+++ tests/server/test_server_mocks.py
@@ -0,0 +1,69 @@
+""" Opentrons simulated/mock tests """  # pylint: disable=protected-access
+from unittest.mock import MagicMock
+from opentrons.simulate import get_protocol_api as get_simulated_protocol_api
+from server.server import ResourceRef, ContextManager
+
+
+def test_load_instrument_none_loaded():
+    context = ContextManager()
+    context._context = MagicMock()
+    ref = ResourceRef('foo', 'bar')
+    context.home(ref)
+    assert context._context.load_instrument.call_args[0][0] == "foo"
+    assert context._context.load_instrument.call_args[0][1] == "bar"
+
+
+def test_load_instrument_replace_head():
+    context = ContextManager()
+    context._context = get_simulated_protocol_api('2.0')
+    assert len(context.instruments) == 0
+
+    ref = ResourceRef('p20_single_gen2', 'right')
+    instrument = context.load_instrument(ref)
+    assert len(context.instruments) == 1
+    assert context.instruments["right"] == instrument
+
+    ref = ResourceRef('p300_single', 'right')
+    instrument_new = context.load_instrument(ref)
+    assert len(context.instruments) == 1
+    assert context.instruments["right"] == instrument_new
+
+
+def test_load_two_heads():
+    context = ContextManager()
+    context._context = get_simulated_protocol_api('2.0')
+    assert len(context.instruments) == 0
+
+    ref1 = ResourceRef('p20_single_gen2', 'right')
+    instrument1 = context.load_instrument(ref1)
+
+    ref2 = ResourceRef('p300_single', 'left')
+    instrument2 = context.load_instrument(ref2)
+
+    assert len(context.instruments) == 2
+    assert context.instruments["right"] == instrument1
+    assert context.instruments["left"] == instrument2
+
+    context.home(ref1)
+    context.home(ref2)
+
+
+def test_reset():
+    context = ContextManager()
+    context._context = get_simulated_protocol_api('2.0')
+    assert len(context.instruments) == 0
+
+    ref1 = ResourceRef('p20_single_gen2', 'right')
+    instrument1 = context.load_instrument(ref1)
+
+    ref2 = ResourceRef('p300_single', 'left')
+    instrument2 = context.load_instrument(ref2)
+
+    assert len(context.instruments) == 2
+    assert context.instruments["right"] == instrument1
+    assert context.instruments["left"] == instrument2
+
+    context.reset()
+
+    assert len(context.instruments) == 0
+    assert context._context is None
